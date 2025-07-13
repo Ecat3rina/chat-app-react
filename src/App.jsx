74e2d1e4a09e355a0451f8d5ce16434c0723398e
ecaterina herta <ecaterinaherta@ecaterinas-MacBook-Air.local>
2025-07-13 12:26:17 +0300

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { ChatControls } from '@components/chat-controls'
+import { ChatDiscussionList } from '@components/chat-discussion-list'
 import './App.css'
 
 function App() {
 
   return (
-    <></>
+    <>
+    <ChatControls/>
+    <ChatDiscussionList/>
+    </>
   )
 }
 
