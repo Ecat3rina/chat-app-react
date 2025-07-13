dd01e9e045b2c5bcc532d0f981263f257be5fb3d
catea <herta.ecaterina@gmail.com>
2025-07-13 19:43:08 +0300

    add constants and some css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { ChatControls } from '@components/chat-controls'
-import { ChatDiscussionList } from '@components/chat-discussion-list'
-import './App.css'
+import "./App.css";
+import { ChatControls } from "@components/chat-controls";
+import { ChatDiscussionList } from "@components/chat-discussion-list";
+import { ChatLayout } from "@components/chat-layout";
+import { ChatMessageList } from "@components/chat-message-list";
+import { ChatStartDiscussionModal } from "@components/chat-start-discussion-modal";
+import { useState } from "react";
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   return (
     <>
-    <ChatControls/>
-    <ChatDiscussionList/>
+      {isModalVisible && <ChatStartDiscussionModal />}
+      <ChatLayout
+        controls={
+          <ChatControls
+            isModalVisible={isModalVisible}
+            setIsModalVisible={setIsModalVisible}
+          />
+        }
+        aside={<ChatDiscussionList />}
+        main={<ChatMessageList />}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/chat-controls.jsx
+++ src/components/chat-controls.jsx
@@ -1,3 +1,12 @@
-export function ChatControls(){
-    return <div>ChatControls</div>
+export function ChatControls({ isModalVisible, setIsModalVisible }) {
+  return (
+    <div className="chat-controls">
+      <button
+        className={`chat-controls-btn ${isModalVisible ? "is-active" : ""}`}
+        onClick={() => setIsModalVisible((prev) => !prev)}
+      >
+        Show modal
+      </button>
+    </div>
+  );
 }
